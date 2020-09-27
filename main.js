4eacb0ccb8c568f76ac23101928232cc124443c1
kh8640 <70800689+kh8640@users.noreply.github.com>
2020-09-27 17:16:25 +0900

    js scr

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+//스크롤링 메뉴
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  {
+    const scrollto = document.querySelector(link);
+    scrollto.scrollIntoView({ behavior: "smooth" });
+  }
+});
